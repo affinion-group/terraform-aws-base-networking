0288b9ea0001ab9aeec599482fcd7a7a49bbbaca
Mark Woodhall <mark.woodhall@gmail.com>
2019-05-30 10:22:06 +0100

    Terraform 0.12 upgrade

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "vpc_cidr" {
   description = "The CIDR to use for the VPC."
 }
+
 variable "region" {
   description = "The region into which to deploy the VPC."
 }
+
 variable "availability_zones" {
   description = "The availability zones for which to add subnets."
 }
@@ -11,21 +13,24 @@ variable "availability_zones" {
 variable "component" {
   description = "The component this network will contain."
 }
+
 variable "deployment_identifier" {
   description = "An identifier for this instantiation."
 }
+
 variable "dependencies" {
   description = "A comma separated list of components depended on my this component."
-  default = ""
+  default     = ""
 }
 
 variable "public_subnets_offset" {
   description = "The number of /24s to offset the public subnets in the VPC CIDR."
-  default = 0
+  default     = 0
 }
+
 variable "private_subnets_offset" {
   description = "The number of /24s to offset the private subnets in the VPC CIDR."
-  default = 0
+  default     = 0
 }
 
 variable "private_zone_id" {
@@ -34,14 +39,16 @@ variable "private_zone_id" {
 
 variable "include_nat_gateway" {
   description = "Whether or not to deploy a NAT gateway for outbound Internet connectivity."
-  default = "yes"
+  default     = "yes"
 }
 
 variable "include_lifecycle_events" {
   description = "Whether or not to notify via S3 of a created VPC."
-  default = "yes"
+  default     = "yes"
 }
+
 variable "infrastructure_events_bucket" {
   description = "S3 bucket in which to put VPC creation events. Required when `include_lifecycle_events` is 'yes'."
-  default = ""
+  default     = ""
 }
+
